f204cb11a5a3a8c8cfaf29e062d74798fcf24428
nansaidh <melanie.wolf_3008@t-online.de>
2019-05-09 15:03:14 +0200

    changed structs to classes, added constructors and destructors

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -28,43 +28,71 @@
 #define START_ROOT (SIZE_FAT + START_FAT)  // 256
 #define START_DATA (SIZE_ROOT + START_ROOT)  // 320
 
-struct SuperBlock {
-    uint16_t size_fs = SIZE_SUPER + SIZE_DMAP + SIZE_FAT + SIZE_ROOT + SIZE_DATA;
-    uint8_t blocksize = BLOCK_SIZE;
-    uint16_t size_data = SIZE_DATA;
-    uint8_t posSuper = START_SUPER;
-    uint8_t posDmap = START_DMAP;
-    uint8_t posFat = START_FAT;
-    uint16_t posRoot = START_ROOT;
-    uint16_t posData = START_DATA;
-    uint8_t currFilesCount;
-    uint8_t currOpenFilesCount;
+class SuperBlock {
+
+    private:
+        uint16_t size_fs = SIZE_SUPER + SIZE_DMAP + SIZE_FAT + SIZE_ROOT + SIZE_DATA;
+        uint8_t blocksize = BLOCK_SIZE;
+        uint8_t posSuper = START_SUPER;
+        uint8_t sizeSuper = SIZE_SUPER;
+        uint8_t posDmap = START_DMAP;
+        uint8_t sizeDmap = SIZE_DMAP;
+        uint8_t posFat = START_FAT;
+        uint8_t sizeFat = SIZE_FAT;
+        uint16_t posRoot = START_ROOT;
+        uint8_t sizeRoot = SIZE_ROOT;
+        uint16_t posData = START_DATA;
+        uint16_t size_data = SIZE_DATA;
+        uint8_t currFilesCount;
+        uint8_t currOpenFilesCount;
+
+    public:
+        SuperBlock();
+        ~SuperBlock();
+
 };
 
-struct Dmap {
-    bool dmap[61440];
+class Dmap {
+    private:
+        bool dmap[61440];
+    public:
+        Dmap();
+        ~Dmap();
 };
 
-struct Fat {
-    uint16_t fat[61140];
+class Fat {
+    private:
+        uint16_t fat[61140];
+    public:
+        Fat();
+        ~Fat();
 };
 
-struct File {
-    char *fileName;
-    uint16_t fileSize;
-    uint32_t userId;
-    uint32_t groupId;
-    uint16_t permissions;
-    // TODO: besseren Typ finden, byteArray ok?
-    uint16_t atime[8]; //letzer Zugriff
+class File {
+    private:
+        char *fileName;
+        uint16_t fileSize;
+        uint32_t userId;
+        uint32_t groupId;
+        uint16_t permissions;
+        // TODO: besseren Typ finden, byteArray ok?
+        uint16_t atime[8]; //letzer Zugriff
 
-    uint16_t mtime[8]; //letze Änderung
-    uint16_t ctime[8]; //letzte Statusänderung
-    uint16_t firstBlock;
+        uint16_t mtime[8]; //letze Änderung
+        uint16_t ctime[8]; //letzte Statusänderung
+        uint16_t firstBlock;
+    public:
+        File();
+        ~File();
+
 };
 
-struct Root {
-    File root[64];
+class Root {
+    private:
+        File root[64];
+    public:
+        Root();
+        ~Root();
 };
 
 
---
+++ src/myfs-structs.cpp
@@ -0,0 +1,6 @@
+//
+// Created by melanie on 09.05.19.
+//
+#include "myfs-structs.h"
+
+
